d3a6416ee01aa96839ed003e997e8fdd1771589b
lucasromanh <lucasromanh@gmail.com>
2024-06-24 02:56:55 -0300

    milkes de cmabios

--- pale-pulsar/astro.config.mjs
+++ pale-pulsar/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      // example: Use tailwind.config.cjs
+      config: './tailwind.config.cjs',
+    }),
+  ],
+});
